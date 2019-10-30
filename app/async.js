14700290911effab7ad08f783c42b9d8d17a2c7a
Christopher Seckler <christopherseckler@christophersmbp.home>
2019-10-30 11:25:55 -0700

    Finish async assessment

--- app/async.js
+++ app/async.js
@@ -2,10 +2,18 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise(resolve => resolve(value));
   },
 
-  manipulateRemoteData: function(url) {
-
+  manipulateRemoteData: async function(url) {
+    // Using promise
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log('data', data);
+        return data.people
+          .map(person => person.name)
+          .sort((a, b) => a.localeCompare(b));
+      });
   }
 };
