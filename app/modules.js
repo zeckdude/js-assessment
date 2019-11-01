f195227feee158c3ab6177f4dd0684d986146284
Christopher Seckler <christopherseckler@christophersmbp.home>
2019-10-31 23:52:31 -0700

    Finish modules assessment

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,12 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-
+    return {
+      greeting: str1,
+      name: str2,
+      sayIt: function() {
+        return `${this.greeting}, ${this.name}`;
+      }
+    };
   }
 };
